c69dc954858137caa205308fcf66a59f2b51aa27
higmito <higordavivideira2009@gmail.com>
2025-10-17 12:40:06 -0300

    Add files via upload

---
+++ projeto/JS/scroll.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.texto-1', {duration: 1000});
+
+sr.reveal('.texto-2', {duration: 1000});
+
+sr.reveal('.texto-3', {duration: 1000});
+
+sr.reveal('.texto-4', {
+    rotate:{x: 0,y: 80,z: 0},
+    duration: 1000
+});
